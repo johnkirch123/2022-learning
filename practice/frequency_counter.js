a2d3b412f76c8849a27c46a16566e72e4c02f1fd
John Kirch <dubskigaming@hotmail.com>
2022-04-12 08:15:21 -0400

    frequency counter principle practice

---
+++ practice/frequency_counter.js
@@ -0,0 +1,20 @@
+function anagramVerification(str1, str2) {
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+
+  for (let char of str1) {
+    frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1;
+  }
+
+  for (let char of str2) {
+    frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
+  }
+
+  for (let key in frequencyCounter1) {
+    if (!(key in frequencyCounter2)) return false;
+    if (frequencyCounter1[key] !== frequencyCounter2[key]) return false;
+  }
+  return true;
+}
+
+console.log(anagramVerification('helloo', 'helloo'));
