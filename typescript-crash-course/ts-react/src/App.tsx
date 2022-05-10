5d791f7fa31be9a468e952bfba5633f394569e50
John Kirch <john.kirch.business@gmail.com>
2022-05-10 09:15:16 -0400

    typescript tut

--- typescript-crash-course/ts-react/src/App.tsx
+++ typescript-crash-course/ts-react/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import List from './components/List';
+import AddToList from './components/AddToList';
 import './App.css';
 
+export interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
+
 function App() {
+  const [people, setPeople] = useState<IState['people']>([
+    {
+      name: 'LeBron James',
+      url: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+      age: 36,
+      note: 'Allergic to staying on same team'
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>People Invited to our Party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
