d6c38e16dc8dbd9542afcc4c29db2c7bf06ae01a
John Kirch <john.kirch.business@gmail.com>
2022-05-19 15:32:54 -0400

    working on rust tutorials for sc's

---
+++ rust/ultimate_rust_crash_course/exercise/b_functions/target/rls/debug/deps/b_functions-5144c803b73cfcea.d
@@ -0,0 +1,5 @@
+c:\Users\dubskiski\Documents\Github Repos\0-learning\2022-learning\rust\ultimate_rust_crash_course\exercise\b_functions\target\rls\debug\deps\b_functions-5144c803b73cfcea.rmeta: src\main.rs
+
+c:\Users\dubskiski\Documents\Github Repos\0-learning\2022-learning\rust\ultimate_rust_crash_course\exercise\b_functions\target\rls\debug\deps\b_functions-5144c803b73cfcea.d: src\main.rs
+
+src\main.rs:
---
+++ rust/ultimate_rust_crash_course/exercise/b_functions/target/rls/debug/deps/b_functions-7a2f387b41c6d84e.d
@@ -0,0 +1,5 @@
+c:\Users\dubskiski\Documents\Github Repos\0-learning\2022-learning\rust\ultimate_rust_crash_course\exercise\b_functions\target\rls\debug\deps\b_functions-7a2f387b41c6d84e.rmeta: src\main.rs
+
+c:\Users\dubskiski\Documents\Github Repos\0-learning\2022-learning\rust\ultimate_rust_crash_course\exercise\b_functions\target\rls\debug\deps\b_functions-7a2f387b41c6d84e.d: src\main.rs
+
+src\main.rs:
