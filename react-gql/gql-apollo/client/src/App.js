0b06ae4bf961d06ca1464f49b2c2a4c6cd230ac9
John Kirch <john.kirch.business@gmail.com>
2022-06-02 19:51:22 -0400

    implementing with react

--- react-gql/gql-apollo/client/src/App.js
+++ react-gql/gql-apollo/client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from
+} from '@apollo/client';
+import { ErrorLink, onError } from '@apollo/client/link/error';
+import GetUsers from './Components/GetUsers';
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+const link = from([
+  errorLink,
+  new HttpLink({ uri: 'http://localhost:5000/graphql' })
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <GetUsers />
+    </ApolloProvider>
   );
 }
 
