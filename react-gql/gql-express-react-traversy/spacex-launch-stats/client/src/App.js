298eb09fbb5f0e2ed42f5fecd75f46861ba85f4e
John Kirch <john.kirch.business@gmail.com>
2022-06-04 07:11:59 -0400

    from 2018 old and broken setup

--- react-gql/gql-express-react-traversy/spacex-launch-stats/client/src/App.js
+++ react-gql/gql-express-react-traversy/spacex-launch-stats/client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { ApolloClient } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import './App.css';
+import Launches from './components/Launches';
+import logo from './logo.PNG';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className='container'>
+        <img
+          src={logo}
+          alt='spaceX logo'
+          style={{ width: 300, display: 'block', margin: 'auto' }}
+        />
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
